9dba45fcb56009958c00b9ed860907275ea857eb
Jeffery Chao <argo1004@JefferydeMacBook-Pro.local>
2025-03-29 15:43:59 -0400

    add user cards

--- lib/main.dart
+++ lib/main.dart
@@ -30,11 +30,52 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: UserSwipeCards()
     );
   }
 }
 
+class UserSwipeCards extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 80), // Top spacing
+          Text(
+            "Users",
+            style: TextStyle(fontSize: 28),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: PageView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return Center(
+                  child: Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Container(
+                      width: 350,
+                      height: 500,
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Page ${index + 1}",
+                        style: TextStyle(fontSize: 32),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -76,15 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -116,6 +149,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
+        backgroundColor: Colors.orangeAccent,
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
